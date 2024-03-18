7940dd0aa661b8258e2f0124c0047be84eebbce7
Kotonoha Akane <akane@akanework.org>
2024-03-18 08:18:30 +0800

    Accord: Remove material color overlays
    Signed-off-by: Kotonoha Akane <akane@akanework.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         // additionally, we (ab)use WindowInsets for bottom sheet padding which won't work on KK
         minSdk = 21 // Android 5.0
         targetSdk = 34 // Android 14.0
-        versionCode = 7
-        versionName = "alpha02"
+        versionCode = 8
+        versionName = "alpha03"
         buildConfigField(
             "String",
             "MY_VERSION_NAME",
