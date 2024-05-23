0afafb949d9f93209f32c6b36cc13547df693ec8
Kotonoha Akane <akane@akanework.org>
2024-05-24 06:16:16 +0800

    Gradle: Fixup codename
    Signed-off-by: Kotonoha Akane <akane@akanework.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,11 +47,11 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 18
-        versionName = "Beta 1"
+        versionName = "beta1"
         buildConfigField(
             "String",
             "MY_VERSION_NAME",
-            "\"$versionName\""
+            "Beta 1"
         )
         buildConfigField(
             "String",
