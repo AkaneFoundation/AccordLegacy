47c4f6b21b8e690535476bcb63e825cc49b8598d
Light summer <93428659+lightsummer233@users.noreply.github.com>
2024-12-13 22:26:33 +0800

    Accord: Bump dependencies, Remove `.idea` `ENABLED_DECODERS=(alac aac ac3 eac3 dts)` `--enable-gpl --enable-version3`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,9 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    kotlin("plugin.parcelize")
     id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.plugin.parcelize")
 }
 
 android {
@@ -20,7 +20,7 @@ android {
     namespace = "org.akanework.gramophone"
     compileSdk = 35
     buildToolsVersion = "35.0.0"
-    ndkVersion = "28.0.12433566-rc1"
+    ndkVersion = "28.0.12674087-rc2"
 
     androidResources {
         generateLocaleConfig = true
@@ -84,23 +84,21 @@ android {
         }
     }
 
-    splits {
-        abi {
-            // Enables building multiple APKs per ABI.
-            isEnable = true
+    splits.abi {
+        // Enables building multiple APKs per ABI.
+        isEnable = true
 
-            // By default all ABIs are included, so use reset() and include to specify that you only
-            // want APKs for x86 and x86_64.
+        // By default all ABIs are included, so use reset() and include to specify that you only
+        // want APKs for x86 and x86_64.
 
-            // Resets the list of ABIs for Gradle to create APKs for to none.
-            reset()
+        // Resets the list of ABIs for Gradle to create APKs for to none.
+        reset()
 
-            // Specifies a list of ABIs for Gradle to create APKs for.
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+        // Specifies a list of ABIs for Gradle to create APKs for.
+        include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
 
-            // Specifies that you don't want to also generate a universal APK that includes all ABIs.
-            isUniversalApk = true
-        }
+        // Specifies that you don't want to also generate a universal APK that includes all ABIs.
+        isUniversalApk = true
     }
 
     buildTypes {
@@ -133,11 +131,11 @@ android {
         includeInApk = false
         includeInBundle = false
     }
+}
 
-    // https://stackoverflow.com/a/77745844
-    tasks.withType<PackageAndroidArtifact> {
-        doFirst { appMetadata.asFile.orNull?.writeText("") }
-    }
+// https://stackoverflow.com/a/77745844
+tasks.withType<PackageAndroidArtifact> {
+    doFirst { appMetadata.asFile.orNull?.writeText("") }
 }
 
 java {
@@ -168,29 +166,29 @@ configurations.configureEach {
 }
 
 dependencies {
-    val media3Version = "1.5.0-rc01"
-    val roomVersion = "2.7.0-alpha11"
+    val media3Version = "1.5.0"
+    val roomVersion = "2.7.0-alpha12"
 
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-ktx:1.10.0-beta01")
+    implementation("androidx.activity:activity-ktx:1.10.0-rc01")
     implementation("androidx.concurrent:concurrent-futures-ktx:1.2.0")
     implementation("androidx.transition:transition-ktx:1.5.1") // <-- for predictive back
     implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.core:core-splashscreen:1.2.0-alpha02")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0-alpha07")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0-alpha08")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.media3:media3-exoplayer:$media3Version")
     implementation("androidx.media3:media3-exoplayer-midi:$media3Version")
     implementation("androidx.media3:media3-session:$media3Version")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("com.google.android.material:material:1.13.0-alpha07")
+    implementation("com.google.android.material:material:1.13.0-alpha08")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("me.zhanghai.android.fastscroll:library:1.3.0")
-    implementation("io.coil-kt.coil3:coil:3.0.3")
+    implementation("io.coil-kt.coil3:coil:3.0.4")
     implementation(files("../libs/lib-decoder-ffmpeg-release.aar"))
     implementation(projects.recyclerview)
     // --- below does not apply to release builds ---
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "8.7.2"
+    val agpVersion = "8.8.0-rc01"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
-    val kotlinVersion = "2.1.0-RC"
-    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.27" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version kotlinVersion apply false
+    val kotlinVersion = "2.1.0"
+    kotlin("android") version kotlinVersion apply false
+    kotlin("plugin.parcelize") version kotlinVersion apply false
+    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.29" apply false
 }
 
 tasks.withType(JavaCompile::class) {
